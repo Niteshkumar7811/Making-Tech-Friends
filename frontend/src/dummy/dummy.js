57d570c92cbec23333ea1787522f2d2849586583
niteshKumar <niteshkumar7811@gmail.com>
2024-07-21 23:35:31 +0530

    this laasy commit

---
+++ frontend/src/dummy/dummy.js
@@ -0,0 +1,48 @@
+export const USERS = [
+	{
+		id: 1,
+		name: "Nitesh Kumar",
+		role: "Creator, Chakra UI",
+		description:
+			"With Chakra UI, I wanted to sync the speed of development with the speed of design. I wanted the developer to be just as excited as the designer to create a screen.",
+	},
+	{
+		id: 2,
+		name: "Rishi Pandey",
+		role: "Co-creator, React Router",
+		description:
+			"I think the biggest thing that React Router tries to do is to make sure that the URL and the UI are always in sync with each other.",
+	},
+	{
+		id: 3,
+		name: "Pawan kumar",
+		role: "Creator, Testing Library",
+		description:
+			"The more your tests resemble the way your software is used, the more confidence they can give you.",
+	},
+	{
+		id: 4,
+		name: "Aman Lal",
+		role: "Creator, Playroom",
+		description:
+			"With Playroom, you can create a zero-install code-oriented design environment that makes it easy to build and share components.",
+	},
+	{
+		id: 5,
+		name: "Suman",
+		role: "SWE 2",
+		description: "I love the web platform and I want to make it as accessible as possible.",
+	},
+	{
+		id: 6,
+		name: "Rohit",
+		role: "SWE 3",
+		description: "I build things and write about it to help you become a better engineer.",
+	},
+	{
+		id: 7,
+		name: "Komal kumari",
+		role: "Senior SWE, Spotify",
+		description: "I'm a software engineer with a passion for building scalable web applications.",
+	},
+];
